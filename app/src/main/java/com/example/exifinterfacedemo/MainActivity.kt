62f94aa9140acf3d32b3856ca6f7e7f2b9a8aaf5
Tejas khartude <tejas.khartude@clariontechnologies.co.in>
2022-04-19 09:52:18 +0530

    Doing ExifInterface

--- app/src/main/java/com/example/exifinterfacedemo/MainActivity.kt
+++ app/src/main/java/com/example/exifinterfacedemo/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.exifinterfacedemo
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.exifinterface.media.ExifInterface
+import com.example.exifinterfacedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var bmp:Bitmap
+    lateinit var scaledBitmap:Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        bmp = BitmapFactory.decodeResource(resources,R.drawable.shibba)
+        val nh = ((bmp.height) * 512.0/ bmp.width)
+        scaledBitmap = Bitmap.createScaledBitmap(bmp,512,nh.toInt(),true)
+        binding.original.setImageBitmap(scaledBitmap)
+
+        binding.btnClick.setOnClickListener {
+
+            letsCompress()
+        }
+
     }
+
+    private fun letsCompress() {
+
+
+
+    }
+
+
 }
